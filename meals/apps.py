55750d15ad025d7621b3bffac162b06792520699
MALAKAHED <lokamalek776@gmail.com>
2025-01-26 22:56:49 +0200

    update app file

---
+++ meals/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MealsConfig(AppConfig): #MealsConfig is used to configure the meals app
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meals'
+    
