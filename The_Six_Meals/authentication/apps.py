981b2f17f9a65dc3f492ca13664ecde6f8bed13c
Amlnasser7 <amlnasser32@gmail.com>
2025-01-27 01:00:32 +0200

    Updated authentication app with new features

--- The_Six_Meals/authentication/apps.py
+++ The_Six_Meals/authentication/apps.py
@@ -3,5 +3,7 @@ from django.apps import AppConfig
 
 
 class AuthConfig(AppConfig):
+    # Specifies the default primary key field type for models in this app
     default_auto_field = 'django.db.models.BigAutoField'
+    # Defines the name of the Django app that will be used in the Django admin interface
     name = 'authentication'
