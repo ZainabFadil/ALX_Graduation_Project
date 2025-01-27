725fb49423dc7d06ddd215616102820c3b41fa66
Zainab Hossien <ugs.19687@ci.suez.edu.eg>
2025-01-27 02:22:58 +0200

    Add comment to document Meals app configuration

--- The_Six_Meals/meals/apps.py
+++ The_Six_Meals/meals/apps.py
@@ -1,5 +1,10 @@
-from django.apps import AppConfig
+# Django App Configuration for Meals App
+# ---------------------------------------
+# This file contains the configuration for the 'meals' app. The MealsConfig 
+# class inherits from AppConfig and defines the app's name and default auto 
+# field type. This is used by Django to initialize the app when the project starts.
 
+from django.apps import AppConfig
 
 class MealsConfig(AppConfig): #MealsConfig is used to configure the meals app
     default_auto_field = 'django.db.models.BigAutoField'
